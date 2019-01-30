7957e2e229784f2ecafd19309e82126b4269b172
Hendrik Vogel <hevo@netlight.com>
2019-01-30 17:23:25 +0100

    added logo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Navigator } from './Navigator';
 import { SessionManager } from './SessionManagement/SessionManager';
 import { HistoryManager } from './HistoryManagement/HistoryManager'
+import logo from './assets/logo.png';
 
 import { colorBranding } from './styles/ColorBranding';
 import Container from 'react-bootstrap/lib/Container';
@@ -16,27 +17,27 @@ import "./styles/mainpage.css";
 document.body.style = 'background: ' + colorBranding.primaryBackground;
 
 class Index extends React.Component {
-    
+
     constructor(props) {
         super(props);
         this.onNavPlayer = this.onNavPlayer.bind(this);
         this.onNavSession = this.onNavSession.bind(this);
         this.onNavHistory = this.onNavHistory.bind(this);
-        this.state = { nav: "player" }
+        this.state = {nav: "player"}
     }
-    
+
     onNavPlayer() {
         this.setState({nav: "player"});
     }
-    
+
     onNavSession() {
         this.setState({nav: "session"});
     }
-    
+
     onNavHistory() {
         this.setState({nav: "history"});
     }
-    
+
     navState() {
         let component = <div/>;
         if (this.state.nav === "player") {
@@ -46,47 +47,50 @@ class Index extends React.Component {
         } else {
             component = <HistoryManager />;
         }
-        
+
         return component;
     }
-    
+
     render() {
         return (
                 (
-                <Container fluid={true} >
-                    <Row>
-                        <Col>
-                        <h1 className="title">
-                            sportsMMR
-                        </h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <h2 className="subtitle">
-                            Competitive for fun.
-                        </h2>
-                        </Col>
-                    </Row>
-                
-                    <Row>
-                        <Navigator onNavPlayer={this.onNavPlayer} onNavSession={this.onNavSession} onNavHistory={this.onNavHistory} />
-                    </Row>
-                
-                    <Row>
-                        <Col> </Col>
-                        <Col xs={10} className="dummy" >
-
+                        <Container fluid={true} >
+                        
+                            <Row>
+                                <Col>
+                                <img  src={logo} className="logo"/>
+                                <h1 className="title">
+                                    sportsMMR
+                                </h1>
+                                </Col>
+                            </Row>
+                        
+                            <Row>
+                                <Col>
+                                <h2 className="subtitle">
+                                    Competitive for fun.
+                                </h2>
+                                </Col>
+                            </Row>
+                        
+                            <Row>
+                                <Navigator onNavPlayer={this.onNavPlayer} onNavSession={this.onNavSession} onNavHistory={this.onNavHistory} />
+                            </Row>
+                        
+                            <Row>
+                                <Col> </Col>
+                                <Col xs={10} className="dummy" >
+                        
                                 { this.navState() }
                         
-                        </Col>
-                        <Col> </Col>               
-                    </Row>
-                </Container>
-                )
+                                </Col>
+                                <Col> </Col>               
+                            </Row>
+                        </Container>
+                        )
                 );
     }
-    
+
 }
 
 
